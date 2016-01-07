6f52506452aac1f577133d2e3d1e2fd11546890a
hztiankai <hztiankai@corp.netease.com>
2016-01-07 10:30:54 +0800

    add total head

---
+++ KLTloadingDemo/KLTLoadingViews/KLTLoadingViews.h
@@ -0,0 +1,16 @@
+//
+//  KLTLoadingViews.h
+//  KLTloadingDemo
+//
+//  Created by 田凯 on 16/1/7.
+//  Copyright © 2016年 netease. All rights reserved.
+//
+
+#ifndef KLTLoadingViews_h
+#define KLTLoadingViews_h
+
+#import "KLTMediaTimingFunctionMgr.h"
+#import "KLTLoadingView.h"
+#import "KLTLoadingStateView.h"
+
+#endif /* KLTLoadingViews_h */
